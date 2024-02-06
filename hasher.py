89e4db5ce678457050847490de138d95cff36ffb
Mariateb <sabmou05@gmail.com>
2024-02-06 10:58:46 +0100

    Ajout TODO class Hasher

--- hasher.py
+++ hasher.py
@@ -1,5 +1,8 @@
 import hashlib
 
 class Hasher:
-    def hash(input: str) -> str:
+    def __init__(self):
+        pass
+    def hash(self, input: str) -> str:
+        # TODO: hashage de l'entrée
         return "plc"
