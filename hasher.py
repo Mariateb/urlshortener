8040ed6cab5c62f4f0af4708dbfe1789605a5f33
Mariateb <sabmou05@gmail.com>
2024-02-06 11:30:46 +0100

    La classe Hasher parvient à correctement hasher une entrée en string

--- hasher.py
+++ hasher.py
@@ -1,8 +1,22 @@
 import hashlib
 
 class Hasher:
+    """
+    Hasher class to easily call to hash a given string (usually an URL).
+
+    :author: Sabri MOUSSA
+    """
     def __init__(self):
-        pass
-    def hash(self, input: str) -> str:
-        # TODO: hashage de l'entrée
-        return "plc"
+        self.hash = hashlib.shake_128()
+
+    def hashString(self, inputString: str, size: int) -> str:
+        """
+        Hash a given input string and return the hash value
+        :param inputString: The string to hash
+        :param size: The size of the resulting hash
+
+        :return hash: The hash value
+        :author: Sabri MOUSSA
+        """
+        self.hash.update(bytes(inputString, 'utf8'))
+        return self.hash.hexdigest(size)
