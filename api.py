60a53b86b3a7788748126e45c7da9ad8525283da
jeremie LOHYER <jeremie.lohyer@alumni.univ-avignon.fr>
2024-02-15 11:32:28 +0100

    gestion erreur integrité base dans create recuperation des infos dans create depuis body (form)
    debut authenticate

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from sqlite3 import IntegrityError
+
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
+from typing import Annotated
 
 import hasher, databaseHandler
 
@@ -9,7 +12,7 @@ app = FastAPI()
 
 
 @app.get("/{shortName}", response_class=RedirectResponse)
-async def reroute(shortName):
+async def redirect(shortName):
     """
     reroute to the website linked or send an error response
     :param shortName: the shortened name
@@ -22,18 +25,29 @@ async def reroute(shortName):
     return HTMLResponse(content="c'est pas bon : "+shortName, status_code=404)
 
 @app.post("/create", response_class=HTMLResponse)
-async def create(url: str = "", size: int = 8):
+async def create(url: Annotated[str, Form()], size: Annotated[int, Form()]):
     """
     allows to create a short link by giving in the post the URL as a parameter
     :param url: the url to shorten
     :param size: the size of the shortened url requested
     :return the response:
     """
+    #TODO: authentification
     myHasher = hasher.Hasher()
     hashed = myHasher.hashString(url, size)
     if url != "" and hashed:
         theDatabase = databaseHandler.DatabaseHandler()
-        if theDatabase.insertLink(url,hashed):
+        try :
+            if theDatabase.insertLink(url,hashed):
+                return HTMLResponse(content=hashed)
+        except IntegrityError:
             return HTMLResponse(content=hashed)
         return HTMLResponse(content="erreur avec la bdd", status_code=500)
+    return HTMLResponse(content="c'est pas bon", status_code=422)
+
+@app.post("/authenticate", response_class=HTMLResponse)
+async def authenticate(login: Annotated[str, Form()], password: Annotated[str, Form()]):
+    # token = databaseHandler.authenticate(login, password)
+    # if token:
+    #     return HTMLResponse(content=token)
     return HTMLResponse(content="c'est pas bon", status_code=422)
